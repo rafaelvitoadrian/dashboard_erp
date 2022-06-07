c4a60c7ece8caaa59257e1009b960bb5fb443fd6
rafaelvitoadrian <87381079+rafaelvitoadrian@users.noreply.github.com>
2022-06-07 23:53:05 +0700

    merubah warna form di css

--- resources/views/admin/user/form_tambah.blade.php
+++ resources/views/admin/user/form_tambah.blade.php
@@ -12,19 +12,19 @@
                 <form action="{{route('user.store')}}" method="POST" class="row g-3">
                   @csrf
                   <div class="col-md-12">
-                    <label for="inputEmail4" class="form-label">Nama</label>
+                    <label for="inputEmail4" class="form-label"><strong>Name</strong></label>
                     <input type="text" class="form-control" id="inputEmail4" name="name" placeholder="Name" required>
                   </div>
                   <div class="col-md-12">
-                    <label for="inputAddress" class="form-label">Email</label>
+                    <label for="inputAddress" class="form-label"><Strong>Email</Strong></label>
                     <input type="email" class="form-control" id="inputAddress" name="email" placeholder="Email" required>
                   </div>
                   <div class="col-md-6">
-                    <label for="inputCity" class="form-label">Password</label>
+                    <label for="inputCity" class="form-label"><strong>Passowrd</strong></label>
                     <input type="password" class="form-control" id="inputCity" name="password" placeholder="Password" required>
                   </div>  
-                  <div class="col-md-6">
-                    <label for="inputState" class="form-label">Role</label>
+                  <div class="col-md-6 mb-1">
+                    <label for="inputState" class="form-label"><Strong>Role</Strong></label>
                     <select id="inputState" name="roles[]" class="form-select">
                       <option selected>Role</option>
                       @foreach($roles as $row)
@@ -33,7 +33,7 @@
                     </select>
                   </div>
                   <div class="col-12">
-                    <button type="submit" class="btn btn-primary">Add</button>
+                    <button type="submit" class="btn btn-primary mr-2">Add</button>
                     <a href="{{route('user.index')}}" class="btn btn-danger">Cancel</a>
                   </div>
                 </form>
--- resources/views/admin/user/form_ubah.blade.php
+++ resources/views/admin/user/form_ubah.blade.php
@@ -4,52 +4,42 @@
 
     <div class="container">
         <div class="card">
-            <div class="card-body">
-                <form class="sign-up-form form" action="{{route('user.update', $user->id)}}" method="POST">
-                    @csrf
-                    @method('PUT')
-                    <h1>Update Account</h1>
-                    <p class="text-medium-emphasis">Update Account</p>
-                    <div class="input-group mb-3"><span class="input-group-text">
-                      <svg class="icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-user"></use>
-                      </svg></span>
-                        <input class="form-control" type="text" value="{{$user->name}}" placeholder="Name" name="name" required>
-                    </div>
-                    <div class="input-group mb-3"><span class="input-group-text">
-                      <svg class="icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-                      </svg></span>
-                        <input class="form-control" type="email" name="email" value="{{$user->email}}" placeholder="Email" required>
-                    </div>
-                    <div class="input-group mb-3"><span class="input-group-text">
-                      <svg class="icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-                      </svg></span>
-                        <input class="form-control" type="text" name="token" value="{{$user->api_token}}" placeholder="Email" >
-                    </div>
-                    <div class="input-group mb-4"><span class="input-group-text">
-                      <svg class="icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
-                      </svg></span>
-                        <select class="form-select" id="inputGroupSelect01" name="roles[]">
-                            @foreach($roles as $row)
+          <div class="card-header py-3">
+            <h1>User Management</h1>
+            <p class="text-medium-emphasis">Update User</p>
+        </div>    
+            <div class="card-body px-4">
+                <form action="{{route('user.update', $user->id)}}" method="POST" class="row g-3">
+                  @csrf
+                  @method('PUT')
+                  <div class="col-md-12">
+                    <label for="inputEmail4" class="form-label"><Strong>Name</Strong></label>
+                    <input type="text" class="form-control" id="inputEmail4" value="{{$user->name}}" name="name" placeholder="Name" required>
+                  </div>
+                  <div class="col-md-12">
+                    <label for="inputAddress" class="form-label"><Strong>Email</Strong></label>
+                    <input type="email" value="{{$user->email}}" class="form-control" id="inputAddress" name="email" placeholder="Email" required>
+                  </div>
+                  <div class="col-md-6 mb-1">
+                    <label for="inputState" class="form-label"><Strong>Role</Strong></label>
+                    <select id="inputState" name="roles[]" class="form-select">
+                      @foreach($roles as $row)
                             <option value="{{$row->id}}" {{$row->id == $user->roles[0]->id ? 'Selected="Selected"' : ''}}>{{$row->name}}</option>
                             @endforeach
-                        </select>
-                    </div>
-                    <div class="input-group mb-4"><span class="input-group-text">
-                      <svg class="icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
-                      </svg></span>
-                        <select class="form-select" id="inputGroupSelect01" name="status">
-                            @foreach(["active" => "Active", "pending" => "Pending","deactive" => "Deactive"] AS $statusWay => $statusLabel)
+                    </select>
+                  </div>
+                  <div class="col-md-6 mb-1">
+                    <label for="inputState1" class="form-label"><Strong>Status</Strong></label>
+                    <select id="inputState1" name="status" class="form-select">
+                       @foreach(["active" => "Active", "pending" => "Pending","deactive" => "Deactive"] AS $statusWay => $statusLabel)
                                 <option value="{{ $statusWay }}" {{ old("contact_way", $user->status) == $statusWay ? "selected" : "" }}>{{ $statusLabel }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <button class="btn btn-block btn-success" type="submit">Update</button>
+                        @endforeach
+                    </select>
+                  </div>
+                  <div class="col-12">
+                    <button type="submit" class="btn btn-primary mr-2">Add</button>
                     <a href="{{route('user.index')}}" class="btn btn-danger">Cancel</a>
+                  </div>
                 </form>
             </div>
         </div>
